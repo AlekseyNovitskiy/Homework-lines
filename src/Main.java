0cc9a5870ca392fd49c9c281b3f37c4d7e732b5b
AlekseyNovitskiy <novickii_aleksei@mail.ru>
2022-09-30 10:13:03 +0300

    ДЗ готово

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Задание 1
+        System.out.println("Задание 1");
+
+        String firstName = "Ivan";
+        firstName = ' '+ firstName;
+        String middleName  = "Ivanovich";
+        middleName = ' ' + middleName;
+        String lastName  = "Ivanov";
+        String fullName = lastName + firstName + middleName;
+        System.out.println("ФИО сотрудника — " + fullName);
+
+        // Задание 2
+        System.out.println("Задание 2");
+
+        String firstNameUpper=firstName.toUpperCase();
+        String middleNameUpper=middleName.toUpperCase();
+        String lastNameUpper=lastName.toUpperCase();
+        fullName = lastNameUpper+firstNameUpper+middleNameUpper;
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName);
+
+        // Задание 3
+        System.out.println("Задание 3");
+
+        fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace("ё", "е");
+        System.out.println("Данные ФИО сотрудника — " + fullName);
+
     }
 }
